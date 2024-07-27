036339a3379a15d1d4c9b46a2aaa353dde7f86cc
abhishekpotbhare01 <abhishek.potbhare01@gmail.com>
2024-07-27 13:58:15 +0530

    project_struture

---
+++ src/main/java/com/egov/EGovBackendApplication.java
@@ -0,0 +1,13 @@
+package com.egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGovBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EGovBackendApplication.class, args);
+    }
+
+}
